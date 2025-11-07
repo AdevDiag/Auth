917130c10c00919b0d672c26f797025a8f7baf4e
adam <kadri.adam.kadri@gmail.com>
2025-11-07 12:47:18 +0100

    add header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,19 @@ import About from './Pages/About'
 import SignUp from './Pages/SignUp'
 import SignIn from './Pages/SignIn'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+      {/* Header*/}
+      <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/profile' element={<Profile/>}/>
-
         </Routes>
       </BrowserRouter>
     </div>
