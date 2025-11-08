0886fe8566213a6b1c822f820ba077b5672d8c1b
adam <kadri.adam.kadri@gmail.com>
2025-11-08 19:40:21 +0100

    complete sign up page functionality

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cors from "cors"
 import userRoutes from "./routes/userRoute.js"
 import authRoutes from './routes/auth.route.js'
 dotenv.config();
@@ -10,6 +11,10 @@ mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log("mongo DB connection is Running!"))
 .catch(error=>console.log(error));
 app.use(express.json());
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}))
 app.use('/api',userRoutes);
 app.use('/api/auth',authRoutes);
 app.listen(port,()=>{
--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useActionState } from 'react'
 import InputField from '../Components/InputField'
 import {Link} from "react-router-dom"
+import axios from "axios"
 const SignUp = () => {
+  const initialState={
+    username:'',
+    email:'',
+    password:'',
+    error:null
+  };
+  const handleLogin=async(previousData,currentFormData)=>{
+    try{
+      const {username,email,password}=Object.fromEntries(currentFormData);
+      const response=await axios.post('http://localhost:8080/api/auth/signup',{username,email,password},{withCredentials:true})
+      const data= await response.data;
+      console.log(data)
+
+    }catch(error){
+      let message=error?.response?.data?.message || error?.message;
+      return {...previousData,error:message}
+    }
+  }
+  const [state,actionFun,isPending]=useActionState(handleLogin,initialState)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold'>Sign Up</h1>
-      <form action="" className='flex flex-col gap-4 p-3'>
+      <form action={actionFun} className='flex flex-col gap-4 p-3'>
         <InputField type="text" placeholder="Username ..." name="username"/>
         <InputField type="text" placeholder="Email ..." name="email"/>
         <InputField type="password" placeholder="Password ..." name="password"/>
 
-        <button className='bg-indigo-700 text-white p-3 rounded-lg uppercase hover:opacity-90 transition-all duration-150 disabled:opacity-80' >Sign up</button>
+        <button 
+        className='bg-indigo-700 text-white p-3 rounded-lg uppercase hover:opacity-90 transition-all duration-150 disabled:opacity-60' 
+        disabled={isPending}>{isPending? 'Checking ...':'Sign up'}</button>
       
       </form>
       <div className='flex gap-2 mt-5'>
@@ -20,6 +42,8 @@ const SignUp = () => {
         </Link>
         
       </div>
+
+      {state.error && <p className=' mt-6 text-red-500 bg-red-200 p-3'>{state.error}</p>}
     </div>
   )
 }
