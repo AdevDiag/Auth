6057a92a685716dbaf18773514c0e3df57c8331f
adam <kadri.adam.kadri@gmail.com>
2025-11-08 11:23:42 +0100

    comment to mongo DB

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 dotenv.config();
 const app=express();
 const port=process.env.PORT;
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log("mongo DB connection is Running!"))
+.catch(error=>console.log(error))
 app.listen(port,()=>{
     console.log(`Server Running on port ${port}`);
 })
