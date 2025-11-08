b52c385d3d18c29df5bb41f384d4aa617a95957e
adam <kadri.adam.kadri@gmail.com>
2025-11-08 20:20:34 +0100

    create Signin route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 export const signup=async(request,response)=>{
     try{
         const {username,email,password}=request.body;
@@ -17,5 +18,26 @@ export const signup=async(request,response)=>{
         }
         let status=errorsMap[error.message] || 500;
         return response.status(status).send({error:true,message: error?.message})
+    }
+}
+export const signin=async(request,response)=>{
+    try{
+        const {email,password}=request.body;
+        const findUser=await userModel.findOne({email});
+        if(!findUser) throw new Error("User not found!");
+        let isPasswordCorrect=await bcrypt.compare(password,findUser.password);
+        if(!isPasswordCorrect) throw new Error("Invalid email or password");
+        const token=jwt.sign({id:findUser._id},process.env.JWT_SECRET,{expiresIn:1000*60*60})
+        return response.status(200).cookie('access_token',token,{httpOnly:true,sameSite:'strict',secure:true,maxAge:1000*60*60}).send({success:true,error:false,message:"User logged In succesfully"});
+    }catch(error){
+        const errorsMap={
+            "User not found!":404,
+            "Invalid email or password":401
+        }
+        let status=errorsMap[error.message];
+        return response.status(status).send({
+            error:true,
+            message:error?.message
+        })
     }
 }
